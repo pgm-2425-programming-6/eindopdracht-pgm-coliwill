89c3254c271d39ec0ade5c71202b27f338bd95ff
pgm-coliwill <coliwill@student.arteveldehs.be>
2025-01-11 16:10:36 +0100

    navbar added

--- quiz-app/src/components/design/navigation/navBar.tsx
+++ quiz-app/src/components/design/navigation/navBar.tsx
@@ -1,12 +1,40 @@
 import { View, StyleSheet, Pressable } from "react-native";
 import React from "react";
-import CrownIcon from "@assets/images/crown.svg"; 
+import CrownIcon from "@assets/images/crown.svg";
+import HomeIcon from "@assets/images/home.svg";
+import LightningIcon from "@assets/images/lightning.svg";
+import { variables } from "@/style/theme";
+import SmallAvatar from "../avatar/SmallAvatar";
 
 const NavBar = () => {
   return (
     <View style={styles.navbar}>
       <Pressable onPress={() => console.log("Home pressed")}>
-        <CrownIcon width={30} height={30} /> 
+        <HomeIcon
+          width={40}
+          height={30}
+          color={variables.colors.navIconActive}
+        />
+      </Pressable>
+
+      <Pressable onPress={() => console.log("Leaderboard pressed")}>
+        <CrownIcon
+          width={40}
+          height={30}
+          color={variables.colors.navIconInactive}
+        />
+      </Pressable>
+
+      <Pressable onPress={() => console.log("Challenges pressed")}>
+        <LightningIcon
+          width={40}
+          height={30}
+          color={variables.colors.navIconInactive}
+        />
+      </Pressable>
+
+      <Pressable onPress={() => console.log("Profile pressed")}>
+        <SmallAvatar />
       </Pressable>
     </View>
   );
@@ -19,9 +47,9 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
-    backgroundColor: "blue", // Replace with your theme color
+    backgroundColor: "white",
     height: 60,
-    borderTopWidth: 1,
-    borderTopColor: "#aaa",
+    borderTopLeftRadius: variables.borderRadius.medium,
+    borderTopRightRadius: variables.borderRadius.medium,
   },
 });
